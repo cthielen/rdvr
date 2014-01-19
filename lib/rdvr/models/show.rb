3bb1010d916daf394fbb842208d47d5b6c3b47d2
Christopher Thielen <cthielen@gmail.com>
2014-01-18 18:51:05 -0800

    Implemented 'rdvr-control edit'

--- lib/rdvr/models/show.rb
+++ lib/rdvr/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
   has_many :recordings, :dependent => :destroy
+  
+  def print_info
+    puts "\tTitle      : '#{self.title}'"
+    puts "\tArtist     : '#{self.artist}'"
+    puts "\tDescription: '#{self.description}'"
+    puts "\tGenre      : '#{self.genre}'"
+    puts "\tWeekly?    : '#{self.weekly}'"
+    puts "\tStart      : '#{self.start}'"
+    puts "\tDuration   : '#{self.duration}'"
+    puts "\tHost       : '#{self.host}'"
+    puts "\tPort       : '#{self.port}'"
+    puts "\tURL        : '#{self.url}'"
+  end
 end
